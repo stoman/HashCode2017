fa54e57beba542e727a2a39c3bb2f2fc9c450a8a
Kirill Martynov <kirmartynov@gmail.com>
2017-02-23 21:49:57 +0100

    new heuristic

--- code/assignservers.cpp
+++ code/assignservers.cpp
@@ -19,13 +19,24 @@ struct Input {
 };
 */
 
-void compute_max_saving(Input &input, int video_id, vector<double> &max_savings, vector<int> &id_max_savings)	{
+void compute_max_saving(Input &input, int video_id, vector<double> &max_savings, vector<int> &id_max_savings, bool rel_cap)	{
 	for (int i = 0; i < input.c; i++)
 	{
-		if ( ((double)input.savings[i][video_id])/* / ((double)input.videosize[video_id])*/ > max_savings[video_id] )
+		if (rel_cap)
+		{	
+			if ( ((double)input.savings[i][video_id]) / ((double)input.videosize[video_id]) > max_savings[video_id] )
+			{
+			   	   max_savings[video_id] = ((double)input.savings[i][video_id]) / ((double)input.videosize[video_id]);
+				id_max_savings[video_id] = i;
+			}
+		}
+		else
 		{
-			   max_savings[video_id] = ((double)input.savings[i][video_id]);// / ((double)input.videosize[video_id]);
-			id_max_savings[video_id] = i;
+			if ( ((double)input.savings[i][video_id]) > max_savings[video_id] )
+			{
+			   	   max_savings[video_id] = ((double)input.savings[i][video_id]);
+				id_max_savings[video_id] = i;
+			}
 		}
 	}
 }
@@ -33,12 +44,16 @@ void compute_max_saving(Input &input, int video_id, vector<double> &max_savings,
 void assignservers(Input& input) {
 	vector<double> max_savings(input.v, 0);
 	vector<int> id_max_savings(input.v, 0);
-  double lastpercent = 0.0;
+  	double lastpercent = 0.0;
+	
+	long long current_volume = 0ll;
+	long long total_volume = (long long)input.x*(long long)input.c;
 	
 	for (int j = 0; j < input.v; j++)
-		compute_max_saving(input,j,max_savings,id_max_savings);
+		compute_max_saving(input,j,max_savings,id_max_savings,(4ll*current_volume >= total_volume));
 	
-	for (int k = 0; k < input.v*input.c; k++)	{
+	int k;
+	for (k = 0; k < input.v*input.c; k++)	{
 		//choose video with maximal savings
 		int maxvideo = 0;
 		for (int j = 1; j < input.v; j++)
@@ -60,13 +75,20 @@ void assignservers(Input& input) {
 			//cerr << "Adding video " << maxvideo << " to server " << server_id << endl;
 			update(input,maxvideo,server_id);
 			
-			
+			current_volume += input.videosize[maxvideo];
 		}
-    if((double)k / (input.v*input.c) > lastpercent + 0.01) {
+		
+		if (4ll*current_volume >= total_volume)
+		{
+			for (int i = 0; i < input.v; i++)
+				compute_max_saving(input,i,max_savings,id_max_savings,(4ll*current_volume >= total_volume));
+		}
+		
+    	if((double)k / (input.v*input.c) > lastpercent + 0.01) {
 		  cerr << "Part done:" << (double)k / (input.v*input.c) << endl;
-      lastpercent += 0.01;
-    }
-	
-		compute_max_saving(input,maxvideo,max_savings,id_max_savings);
+      	lastpercent += 0.01;
+		}
+		
+		compute_max_saving(input,maxvideo,max_savings,id_max_savings,(4ll*current_volume >= total_volume));
 	}
 }
