1e590213156dad2726216a131b3b53e1cc06de46
Stefan Toman <git@stoman.de>
2017-02-23 19:46:45 +0100

    add input reading

--- code/util.cpp
+++ code/util.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iostream>
 #include <limits>
+#include <map>
 #include <string>
 #include <time.h>
 #include <utility>
@@ -12,15 +13,40 @@
 using namespace std;
 
 //util classes
-struct SomeClass {
+struct Endpoint {
+  int s;//latency to data center
+  map<int, int> requests;//first int video id, second int number of requests
+  map<int, int> connections;//first int is cache server id, second int is latency
 };
 
 struct Input {
+  int v, e, r, c, x;
+  vector<Endpoint> endpoints;
+  vector<int> videosize;
 };
 
 //input handling
 void readInput(Input& input, istream& in) {
-	//TODO read input
+	in >> input.v >> input.e >> input.r >> input.c >> input.x;
+  input.endpoints.resize(input.e);
+  input.videosize.resize(input.v);
+  for(int i = 0; i < input.v; i++) {
+    in >> input.videosize[i];
+  }
+  for(int i = 0; i < input.e; i++) {
+    int k;
+    in >> input.endpoints[i].s >> k;
+    for(int j = 0; j < k; j++) {
+      int c, l;
+      in >> c >> l;
+      input.endpoints[i].connections[c] = l;
+    }
+  }
+  for(int i = 0; i < input.r; i++) {
+    int v, e, n;
+    in >> v >> e >> n;
+    input.endpoints[e].requests[v] += n;
+  }
 }
 
 #endif
